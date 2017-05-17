5fad55f489b9238601a783910d57083580f41095
Evan Kaplan <evnkpln@gmail.com>
2017-05-17 00:02:40 +0000

    Assignment 20: Add question functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@ require 'random_data'
         body: RandomData.random_paragraph
         )
 end
+
+50.times do
+    r = rand(0..1)
+    seedBool = r == 0
+    Question.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        resolved: seedBool
+        )
+end
+
 posts = Post.all
 
 100.times do
@@ -17,4 +28,5 @@ end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
+puts "#{Question.count} questions created"
 puts "#{Comment.count} comments created"
