f768706a9ca446b2a81c10f0fd46912b79bd1a2b
Evan Kaplan <evnkpln@gmail.com>
2017-06-06 06:49:26 +0000

    Checkpoint 28: Add admin and authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604202721) do
+ActiveRecord::Schema.define(version: 20170606041649) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170604202721) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
