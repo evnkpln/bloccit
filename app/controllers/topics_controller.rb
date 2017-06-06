e6c4f326f6dda142492d9c0bf3e5a6c06c551224
Evan Kaplan <evnkpln@gmail.com>
2017-06-06 22:12:53 +0000

    Assignment 28: Add moderator role and associated functions.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
     
     before_action :require_sign_in, except: [:index, :show]
-    before_action :authorize_user, except: [:index, :show]
+    before_action :authorize_user, except: [:index, :show, :update, :edit]
+    before_action :authorize_for_edit, only: [:update, :edit]
     
     def index
         @topics = Topic.all
@@ -63,6 +64,12 @@ class TopicsController < ApplicationController
     
     def authorize_user
         unless current_user.admin?
+            flash[:alert] = "You must be an admin to do that."
+            redirect_to topics_path
+        end
+    end
+    def authorize_for_edit
+        unless current_user.admin? || current_user.moderator?
             flash[:alert] = "You must be an admin to do that."
             redirect_to topics_path
         end
